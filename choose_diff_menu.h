e6ef0242164260932039d23edc236e82ae2ad70f
Ivan Honcharuk <ivan.honcharuk@student.tuke.sk>
2023-12-10 11:49:02 +0100

    diffs

--- choose_diff_menu.h
+++ choose_diff_menu.h
@@ -7,16 +7,176 @@
 #include <curses.h>
 #include <time.h>
 
+void on_click_snail()
+{
+    mvprintw(16, 67, "+---------------------+");
+    mvprintw(17, 67, "|");
+    attron(COLOR_PAIR(9));
+    mvprintw(17, 68, "   Slow as a snail   ");
+    attroff(COLOR_PAIR(9));
+    mvprintw(17, 89, "|");
+    mvprintw(18, 67, "+---------------------+");
 
+    mvprintw(19, 67, "+---------------------+");
+    mvprintw(20, 67, "|         Easy        |");
+    mvprintw(21, 67, "+---------------------+");
+
+    mvprintw(22, 67, "+---------------------+");
+    mvprintw(23, 67, "|        Normal       |");
+    mvprintw(24, 67, "+---------------------+");
+
+    mvprintw(25, 67, "+---------------------+");
+    mvprintw(26, 67, "|         Hard        |");
+    mvprintw(27, 67, "+---------------------+");
+    move(0,0);
+}
+void on_click_easy()
+{
+    mvprintw(16, 67, "+---------------------+");
+    mvprintw(17, 67, "|   Slow as a snail   |");
+    mvprintw(18, 67, "+---------------------+");
+
+    mvprintw(19, 67, "+---------------------+");
+    mvprintw(20, 67, "|");
+    attron(COLOR_PAIR(9));
+    mvprintw(20, 68, "         Easy        ");
+    attroff(COLOR_PAIR(9));
+    mvprintw(20, 89, "|");
+    mvprintw(21, 67, "+---------------------+");
+
+    mvprintw(22, 67, "+---------------------+");
+    mvprintw(23, 67, "|        Normal       |");
+    mvprintw(24, 67, "+---------------------+");
+
+    mvprintw(25, 67, "+---------------------+");
+    mvprintw(26, 67, "|         Hard        |");
+    mvprintw(27, 67, "+---------------------+");
+    move(0,0);
+}
+void on_click_normal()
+{
+    mvprintw(16, 67, "+---------------------+");
+    mvprintw(17, 67, "|   Slow as a snail   |");
+    mvprintw(18, 67, "+---------------------+");
+
+    mvprintw(19, 67, "+---------------------+");
+    mvprintw(20, 67, "|         Easy        |");
+    mvprintw(21, 67, "+---------------------+");
+
+    mvprintw(22, 67, "+---------------------+");
+    mvprintw(23, 67, "|");
+    attron(COLOR_PAIR(9));
+    mvprintw(23, 68, "        Normal       ");
+    attroff(COLOR_PAIR(9));
+    mvprintw(23, 89, "|");
+    mvprintw(24, 67, "+---------------------+");
+
+    mvprintw(25, 67, "+---------------------+");
+    mvprintw(26, 67, "|         Hard        |");
+    mvprintw(27, 67, "+---------------------+");
+
+    move(0,0);
+}
+void on_click_hard()
+{
+    mvprintw(16, 67, "+---------------------+");
+    mvprintw(17, 67, "|   Slow as a snail   |");
+    mvprintw(18, 67, "+---------------------+");
+
+    mvprintw(19, 67, "+---------------------+");
+    mvprintw(20, 67, "|         Easy        |");
+    mvprintw(21, 67, "+---------------------+");
+
+    mvprintw(22, 67, "+---------------------+");
+    mvprintw(23, 67, "|        Normal       |");
+    mvprintw(24, 67, "+---------------------+");
+
+    mvprintw(25, 67, "+---------------------+");
+    mvprintw(26, 67, "|");
+    attron(COLOR_PAIR(9));
+    mvprintw(26, 68, "         Hard        ");
+    attroff(COLOR_PAIR(9));
+    mvprintw(26, 89, "|");
+    mvprintw(27, 67, "+---------------------+");
+
+    move(0,0);
+}
 
 int choose_difficulty()
 {
-    mvprintw(13, 60, "Please, choose game difficulty.");
-    mvprintw(15, 64, "There is 4 difficulties:");
-    mvprintw(16, 64, "Slow as a snail");
-    mvprintw(17, 64, "Easy");
-    mvprintw(18, 64, "Normal");
-    mvprintw(19, 64, "Hard");
-    getch();
-    return 2;
+    mvprintw(13, 63, "Please, choose game difficulty.");
+    mvprintw(15, 66, "There is 4 difficulties:");
+    on_click_snail();
+    int wait;
+    int pos = 1;
+
+    fflush(stdin);
+    do
+    {
+        wait = getch();
+        if (wait == 'W' || wait == 'w' || wait == KEY_UP)
+        {
+            if (pos > 1)
+            {
+                pos--;
+            }
+            switch (pos)
+            {
+                case 1:
+                    on_click_snail();
+                    move(0, 0);
+                    break;
+                case 2:
+                    on_click_easy();
+                    move(0, 0);
+                    break;
+                case 3:
+                    on_click_normal();
+                    move(0, 0);
+                    break;
+                case 4:
+                    on_click_hard();
+                    move(0, 0);
+                    break;
+                default:
+                    on_click_snail();
+                    move(0, 0);
+            }
+        }
+        else if (wait == 'S' || wait == 's' || wait == KEY_DOWN)
+        {
+            if (pos < 4)
+            {
+                pos++;
+            }
+            switch (pos)
+            {
+                case 1:
+                    on_click_snail();
+                    move(0, 0);
+                    break;
+                case 2:
+                    on_click_easy();
+                    move(0, 0);
+                    break;
+                case 3:
+                    on_click_normal();
+                    move(0, 0);
+                    break;
+                case 4:
+                    on_click_hard();
+                    move(0, 0);
+                    break;
+                default:
+                    on_click_snail();
+                    move(0, 0);
+            }
+        }
+        else if (wait == '\n')
+        {
+            return 5 - pos;
+        }
+    }while (wait != '\n');
+
+    return pos;
 }
